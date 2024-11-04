7a96062ef7dedebbb39debd36fd69b68739b2170
Maria Estrella <98559654+mlestrella843@users.noreply.github.com>
2024-11-04 15:46:26 -0700

    Handlers

--- calculatorapp/src/App.js
+++ calculatorapp/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
+
 function App() {
+  const [num1, setNum1] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setNum1(value ? parseInt(value) : 0);
+  };
+
+  const handleAddition = () => setTotal(total + num1);
+  const handleSubtraction = () => setTotal(total - num1);
+  const handleMultiplication = () => setTotal(total * num1);
+  const handleDivision = () => {
+    if (num1 !== 0) {
+      setTotal(total / num1);
+    } else {
+      alert("Cannot divide by 0");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calculator App</h1>
+      <input
+        type="number"
+        value={num1 || ''}
+        onChange={handleInputChange}
+      />
+
+      <h3>Total: {total}</h3>
+
+      <button onClick={handleAddition} className="btn-primary">Add</button>
+      <button onClick={handleSubtraction} className="btn-primary">Subtract</button>
+      <button onClick={handleMultiplication} className="btn-primary">Multiply</button>
+      <button onClick={handleDivision} className="btn-primary">Divide</button>
     </div>
   );
 }
